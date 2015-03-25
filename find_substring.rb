e1a5791120230ee19a0dcb0d35726c127b7a8ec0
Noel Bacani <mail@ndovutech.com>
2015-03-24 17:50:41 -0700

    This is the second commit...maybe...?

---
+++ find_substring.rb
@@ -0,0 +1,13 @@
+# find_substring.rb
+
+# Given a string
+# my_string = "Hello World" 
+# Find out a way to get a substring that contains the last 4 characters.
+
+my_string = "Hello World" 
+
+if my_string.size > 4
+	run_get_string_method
+else
+	# do nothing
+end
